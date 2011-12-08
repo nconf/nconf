e94a5aad604a22e722afc5cf2d9ba8907f82f107
Angelo Gargiulo <agargiulo@nconf.org>
2011-12-08 02:08:34 +0100

    changed some messages

--- include/messages.php
+++ include/messages.php
@@ -8,13 +8,13 @@ define("TXT_HISTORY_SUBTITLE",      "For a more fine-graned history listing, che
 
 define("TXT_LOGIN",                 "Please login with your ".AUTH_TYPE." account.");
 define("TXT_LOGIN_FAILED",          "Authentication failed");
-define("TXT_LOGIN_NOT_AUTHORIZED",  "User authenticated, but its group is not authorized to access this tool");
+define("TXT_LOGIN_NOT_AUTHORIZED",  "User authenticated, but his group is not authorized to access NConf");
 define("TXT_GO_BACK_BUTTON",        "Please go back using the back button!");
 define("TXT_UPDATE_SERVICES",       "Would you like to update 'check period', 'notification period' <br>and 'contact groups' of all services assigned to this host?");
 define("TXT_UPDATE_SERVICES_LOOSING_VALUES", "These values will be removed from its services if you update your services according to the host:");
 define("TXT_DELETE_CHILD_SERVICES", "The following services will also be deleted:");
 define("TXT_DB_NO_WRITES",          '<font color="red"><b>DB_NO_WRITES = 1:<br>No DB inserts, modifications or deletions will be performed</b></font>');
-define("TXT_NO_RESENT",             "Sorry, the submited infos are not allowed to be resent. Go to the form and submit it.");
+define("TXT_NO_RESENT",             "Sorry, the submited data is not allowed to be resent. Go to the form and submit it.");
 define("TXT_DELETED",               "Successfully deleted");
 define("TXT_SUBMIT_DISABLED4USER",  "Submit disabled, you are not allowed to add/modify/delete Admin users!");
 
@@ -24,7 +24,7 @@ define("TXT_NAMING_ATTR_LAST",      SHOW_ATTR_NAMING_ATTR_CONFLICT." ATTENTION: 
 define("TXT_NAMING_ATTR_CHANGED",   SHOW_ATTR_NAMING_ATTR_CONFLICT." ATTENTION: New 'naming attribute' set. Previous one changed, because only one 'naming attribute' is allowed!".SHOW_ATTR_NAMING_ATTR_CONFLICT);
 define("TXT_DEPVIEW_ERROR_LOOP",    "Warning: circular parent/child chain detected");
 
-define("CHECKCOMMAND_ORDER_PRETEXT",                    "Current list is orderer by:");
+define("CHECKCOMMAND_ORDER_PRETEXT",                    "Current list is ordered by:");
 define("CHECKCOMMAND_ORDER_COMMAND_NAME_ASC",           "checkcommand name (ascending)");
 define("CHECKCOMMAND_ORDER_COMMAND_NAME_DESC",          "checkcommand name (descending)");
 define("CHECKCOMMAND_ORDER_DEFAULT_SERVICE_NAME_ASC",   "default service name (ascending)");
