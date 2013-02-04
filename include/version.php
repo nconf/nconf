fee4e3d5d39e08d373a93ab8607bf2cd03df407b
Fabian Gander <gander@jumps.ch>
2013-02-05 00:14:45 +0100

    changed version number back to 1.3.1

--- include/version.php
+++ include/version.php
@@ -2,11 +2,11 @@
 #
 # File for Version number and info
 #
-define("VERSION_NUMBER", '1.4.0');
+define("VERSION_NUMBER", '1.3.1');
 define("VERSION_STRING", '<img src="design_templates/nconf_fresh/nconf-logo-16.gif" style="vertical-align:bottom" alt="nconf-logo">
                           <b>NConf v. '.VERSION_NUMBER.' (dev)</b>');
 
-define("VERSION_NOT_FINAL_WARNING", 'This is a pre-release version of NConf 1.4.0, intended FOR TESTING PURPOSES ONLY! 
+define("VERSION_NOT_FINAL_WARNING", 'This is a pre-release version of NConf '.VERSION_NUMBER.', intended FOR TESTING PURPOSES ONLY! 
                                     <br>Do not use this version in a production environment or to upgrade your productive NConf installation!
                                     <br>Note that we will not provide any update functionality from this pre-release to the next official release.');
 
